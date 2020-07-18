1fce7c76be160ccdec1df874be006bbb5aa03f52
srj_b <srj.belokon@gmail.com>
2020-07-18 10:50:09 +0300

    refactored rotation script

--- dotfiles_rotation.sh
+++ dotfiles_rotation.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
+
+env_dir="${HOME}/Documents/env"
+platform=`uname`
+
+declare -a rotation_files=(".tmux.conf" ".bash_profile" ".zshrc"
+    ".vimrc" ".local" ".ssh" ".aws"
+)
+
+firefox_profile_home="${HOME}/Library/Application Support/Firefox/Profiles"
+firefox_profile_name=`ls "${firefox_profile_home}"`
+firefox_cfg="${firefox_profile_home}/${firefox_profile_name}/user.js"
+
 main() {
-    local env_dir="$HOME/Documents/env"
-    local rotation_files=".tmux.conf .bash_profile .zshrc .vimrc .local .ssh .aws"
 
-    local firefox_profile_home="$HOME/Library/Application Support/Firefox/Profiles"
-    local firefox_profile_name=$(ls "$firefox_profile_home")
-    
-    local firefox_cfg="$firefox_profile_home/$firefox_profile_name/user.js"
-
-    for dotf in $rotation_files; do
-        local envf="$(echo $dotf |sed 's/.//')"
-        rm -rf "$env_dir/dotfiles/$envf" #cleanup
-        cp -fR "$HOME/$dotf" "$env_dir/dotfiles"
-        mv "$env_dir/dotfiles/$dotf" "$env_dir/dotfiles/$envf"
+    for dotf in ${rotation_files[@]}; do
+        rm -rf "${env_dir}/dotfiles/${platform}/${dotf:1}" #cleanup
+        cp -fR "${HOME}/${dotf}" "${env_dir}/dotfiles/${platform}/${dotf:1}"
     done
 
-    cp "$firefox_cfg" "$env_dir/firefox"
+    cp "${firefox_cfg}" "${env_dir}/firefox"
 
-    brew bundle dump && mv Brewfile "$env_dir/homebrew"
+    brew bundle dump && mv Brewfile "${env_dir}/homebrew"
 
-    pip3 freeze >"$env_dir/pip3/requirements.txt"
+    pip3 freeze >"${env_dir}/pip3/requirements.txt"
+
+    cd "${env_dir}" && git commit -am "[${0}] files has been rotated." && git push
 }
 
 
