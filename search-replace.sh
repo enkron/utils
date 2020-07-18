83eec807f5af14f7feb446e459568c5d73c397a5
srj_b <srj.belokon@gmail.com>
2020-07-18 11:18:25 +0300

    refactored script

--- search-replace.sh
+++ search-replace.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
-main()
-{
-    local old_text="$1"
-    local new_text="$2"
 
-    egrep -lRi "$old_text" "$PWD"/* |xargs sed -e "s/"$old_text"/"$new_text"/g" -i ''
+printUsage() {
+    printf "${0} -o <old_text> -n <new_text>\n"
+    exit 1
 }
 
-main "$1" "$2"
+main() {
+#swap <old_text> with <new_one> recursively in the project tree
+
+    while getopts "o:n:" flags; do
+        case "${flags}" in
+            o) oldText=${OPTARG} ;;
+
+            n) newText=${OPTARG} ;;
+
+            *) printUsage ;;
+        esac
+    done
+    shift $(( $OPTIND-1 ))
+
+    if [ -z "${oldText}" ] || [ -z "${newText}" ]; then
+        printUsage
+    fi
+
+    echo "${oldText}"
+    echo "${newText}"
+
+    egrep -lRi "${oldText}" "${PWD}"/* |xargs sed -e "s/"${oldText}"/"${newText}"/g" -i ''
+}
+
+main "${@}"
