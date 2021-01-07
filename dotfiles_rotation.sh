557ca4e17d560849536600ef9bd0137e43ebf9a8
Sergei Belokon <srj.belokon@gmail.com>
2021-01-07 11:01:44 +0300

    Refactored script
     	modified:   dotfiles_rotation.sh

--- dotfiles_rotation.sh
+++ dotfiles_rotation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-env_dir="${HOME}/Documents/env"
-platform=`uname`
+ENV_DIR="${HOME}/Documents/env"
+PLATFORM=`uname`
 
 declare -a rotation_files=(
     '.tmux.conf'
@@ -20,22 +20,17 @@ firefox_profile_home="${HOME}/Library/Application Support/Firefox/Profiles"
 firefox_profile_name=`ls "${firefox_profile_home}"`
 firefox_cfg="${firefox_profile_home}/${firefox_profile_name}/user.js"
 
-main() {
+for dotf in ${rotation_files[@]}; do
+    rm -rf "${ENV_DIR}/dotfiles/${PLATFORM}/${dotf:1}" #cleanup
+    cp -fR "${HOME}/${dotf}" "${ENV_DIR}/dotfiles/${PLATFORM}/${dotf:1}"
+done
 
-    for dotf in ${rotation_files[@]}; do
-        rm -rf "${env_dir}/dotfiles/${platform}/${dotf:1}" #cleanup
-        cp -fR "${HOME}/${dotf}" "${env_dir}/dotfiles/${platform}/${dotf:1}"
-    done
+cp "${firefox_cfg}" "${ENV_DIR}/firefox"
 
-    cp "${firefox_cfg}" "${env_dir}/firefox"
+brew bundle dump && mv Brewfile "${ENV_DIR}/homebrew"
 
-    brew bundle dump && mv Brewfile "${env_dir}/homebrew"
+pip3 freeze >"${ENV_DIR}/pip3/requirements.txt"
 
-    pip3 freeze >"${env_dir}/pip3/requirements.txt"
+cd "${ENV_DIR}"
+git add . && git commit -m "[${0}] files has been rotated." && git push
 
-    cd "${env_dir}"
-    git add . && git commit -m "[${0}] files has been rotated." && git push
-}
-
-
-main
